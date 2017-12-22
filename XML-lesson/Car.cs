8739376744a85fa599f680f3bee0cd6e2225fdcc
ZhaslanR <zhaslan_12@bk.ru>
2017-12-22 16:33:17 +0600

    Добавьте файлы проекта.

---
+++ XML-lesson/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_lesson
+{
+    public class Car
+    {
+        public Guid Id { get; set; }
+        public string Mark { get; set; }
+        public int Rating { get; set; }
+        public DateTime MadeDate { get; set; }
+    }
+}
---
+++ XML-lesson/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XML_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            XmlDocument document = new XmlDocument();
+            document.Load("Cars.xml");
+
+            var rootElement = document.LastChild;
+
+            XmlElement newCarElement = document.CreateElement("car");
+            rootElement.AppendChild(newCarElement);
+
+            document.Save("Cars.xml");
+
+            foreach (XmlElement element in rootElement)
+            {
+
+                Car car = new Car();
+                car.Id = Guid.Parse(element.ChildNodes[0].InnerText);
+                car.Mark = element.ChildNodes[1].InnerText;
+                car.MadeDate = DateTime.Parse(element.ChildNodes[2].InnerText);
+                car.Rating = int.Parse(element.ChildNodes[3].Attributes["value"].InnerText);
+            }
+
+
+        }
+    }
+}
